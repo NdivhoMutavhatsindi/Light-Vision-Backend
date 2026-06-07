0756da80e12c1e327b63576be73c4a8b16898b3b
DrMutavhatsindi <ndivhomutavhatsindi@icloud.com>
2026-06-07 04:03:30 +0200

    Update

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,5 @@
-import { PrismaClient } from "@prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
-import pg from "pg"
-import dotenv from "dotenv"
-import path from "path"
-import { fileURLToPath } from "url"
+import { PrismaClient } from "@prisma/client";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-dotenv.config({ path: path.resolve(__dirname, "../../.env") })
+const prisma = new PrismaClient();
 
-const connectionString = process.env.DATABASE_URL
-
-const pool = new pg.Pool({
-  connectionString,
-})
-
-const adapter = new PrismaPg(pool)
-
-const prisma = new PrismaClient({
-  adapter,
-})
-
-export default prisma
+export default prisma;
