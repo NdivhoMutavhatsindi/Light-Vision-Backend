cde0ff6774335805e44b29be4c3e61b2d6aafd41
DrMutavhatsindi <ndivhomutavhatsindi@icloud.com>
2026-07-07 11:02:57 +0200

    update

--- tmp_prisma_enum2.mjs
+++
@@ -1,3 +0,0 @@
-import * as Client from '@prisma/client';
-console.log('PropertyStatus (top-level export):', Client.PropertyStatus);
-console.log('Prisma.$Enums.PropertyStatus:', Client.Prisma?.$Enums?.PropertyStatus);
--- tmp_prisma_keys.mjs
+++
@@ -1,3 +0,0 @@
-import { Prisma } from '@prisma/client';
-console.log(Object.keys(Prisma));
-console.log(JSON.stringify(Prisma, null, 2));
