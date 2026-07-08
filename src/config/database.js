e9385584c4df434d92341b0ad5d095204a9d8dc9
DrMutavhatsindi <ndivhomutavhatsindi@icloud.com>
2026-07-08 11:38:21 +0200

    updated

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,19 @@
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: resolve(__dirname, "../../.env") });
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 export default prisma;
